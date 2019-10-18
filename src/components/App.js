cf42920100b4d82e83ff4e58d1944cb4e886bf40
hamzanadeveloper <hamza.arshad.developer@gmail.com>
2019-10-17 20:02:38 -0400

    Forgot to import the useReducer

--- src/components/App.js
+++ src/components/App.js
@@ -1,101 +1,79 @@
-import React, { useState, useEffect } from "react";
-import "../App.css";
+import React, { useReducer, useEffect } from "react";
+
 import Header from "./Header";
 import Movie from "./Movie";
+import spinner from "../assets/ajax-loader.gif";
 import Search from "./Search";
+import { initialState, reducer } from "../store/reducer";
+import axios from "axios";
 
-
-const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=7b6d1c01"; // you should replace this with yours
-
-const initialState = {
-  loading: true,
-  movies: [],
-  errorMessage: null
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "SEARCH_MOVIES_REQUEST":
-      return {
-        ...state,
-        loading: true,
-        errorMessage: null
-      };
-    case "SEARCH_MOVIES_SUCCESS":
-      return {
-        ...state,
-        loading: false,
-        movies: action.payload
-      };
-    case "SEARCH_MOVIES_FAILURE":
-      return {
-        ...state,
-        loading: false,
-        errorMessage: action.error
-      };
-    default:
-      return state;
-  }
-};
+const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=7b6d1c01";
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+    const [state, dispatch] = useReducer(reducer, initialState);
 
-  useEffect(() => {
-
-    fetch(MOVIE_API_URL)
-        .then(response => response.json())
-        .then(jsonResponse => {
-
-          dispatch({
-            type: "SEARCH_MOVIES_SUCCESS",
-            payload: jsonResponse.Search
-          });
-        });
-  }, []);
-
-  const search = searchValue => {
-    dispatch({
-      type: "SEARCH_MOVIES_REQUEST"
-    });
-
-    fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=4a3b711b`)
-        .then(response => response.json())
-        .then(jsonResponse => {
-          if (jsonResponse.Response === "True") {
+    useEffect(() => {
+        axios.get(MOVIE_API_URL).then(jsonResponse => {
             dispatch({
-              type: "SEARCH_MOVIES_SUCCESS",
-              payload: jsonResponse.Search
+                type: "SEARCH_MOVIES_SUCCESS",
+                payload: jsonResponse.data.Search
             });
-          } else {
-            dispatch({
-              type: "SEARCH_MOVIES_FAILURE",
-              error: jsonResponse.Error
-            });
-          }
         });
-  };
+    }, []);
 
-  const { movies, errorMessage, loading } = state;
+    // you can add this to the onClick listener of the Header component
+    const refreshPage = () => {
+        window.location.reload();
+    };
 
-  return (
-      <div className="App">
-        <Header text="HOOKED" />
-        <Search search={search} />
-        <p className="App-intro">Sharing a few of our favourite movies</p>
-        <div className="movies">
-          {loading && !errorMessage ? (
-              <span>loading... </span>
-          ) : errorMessage ? (
-              <div className="errorMessage">{errorMessage}</div>
-          ) : (
-              movies.map((movie, index) => (
-                  <Movie key={`${index}-${movie.Title}`} movie={movie} />
-              ))
-          )}
+    const search = searchValue => {
+        dispatch({
+            type: "SEARCH_MOVIES_REQUEST"
+        });
+
+        axios(`https://www.omdbapi.com/?s=${searchValue}&apikey=7b6d1c01`).then(
+            jsonResponse => {
+                if (jsonResponse.data.Response === "True") {
+                    dispatch({
+                        type: "SEARCH_MOVIES_SUCCESS",
+                        payload: jsonResponse.data.Search
+                    });
+                } else {
+                    dispatch({
+                        type: "SEARCH_MOVIES_FAILURE",
+                        error: jsonResponse.data.Error
+                    });
+                }
+            }
+        );
+    };
+
+    const { movies, errorMessage, loading } = state;
+
+    const retrievedMovies =
+        loading && !errorMessage ? (
+            <img className="spinner" src={spinner} alt="Loading spinner" />
+        ) : errorMessage ? (
+            <div className="errorMessage">{errorMessage}</div>
+        ) : (
+            movies.map((movie, index) => (
+                <Movie key={`${index}-${movie.Title}`} movie={movie} />
+            ))
+        );
+
+    return (
+        <div className="App">
+            <div className="m-container">
+                <Header text="HOOKED" />
+
+                <Search search={search} />
+
+                <p className="App-intro">Sharing a few of our favourite movies</p>
+
+                <div className="movies">{retrievedMovies}</div>
+            </div>
         </div>
-      </div>
-  );
+    );
 };
 
 export default App;
